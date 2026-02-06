9f87cbd8f059dd9d2b1dcbfb0e97541b51d97e0b
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-02-06 21:17:34 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index
+
+  devise_for :users   # ← これを追加
 
   root to: "messages#index"
   
